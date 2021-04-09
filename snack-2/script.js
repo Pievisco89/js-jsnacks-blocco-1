a76923ff69508ad24b7982916582c2369a391d28
Pievisco89 <pie-89@live.it>
2021-04-09 10:47:53 +0200

    snack-2 corretto

--- snack-2/script.js
+++ snack-2/script.js
@@ -1,12 +1,12 @@
 var parola1 = prompt("Inserisci la prima parola");
 var parola2 = prompt("Inserisci la seconda parola");
 
-if(parola1 > parola2){
-  console.log(parola1);
+if(parola1.length > parola2.length){
   console.log(parola2);
-}else if(parola1 === parola2){
+  console.log(parola1);
+}else if(parola1.length === parola2.length){
   console.log("Le parole sono lunghe uguali");
 }else{
-  console.log(parola2);
   console.log(parola1);
+  console.log(parola2);
 }
