7b025ca64e38cf4f11458700e3cdc52a8c4938e4
Pievisco89 <pie-89@live.it>
2021-04-13 17:02:02 +0200

    snack 9 fatto

--- snack-9/script.js
+++ snack-9/script.js
@@ -1,1 +1,13 @@
-console.log("CIaoacicico")
+/* creo variabili somma e media */
+var somma = 0;
+
+/* ciclo for per calcoli */
+for(var i = 1; i <= 10; i++){
+  var num_somma = i;
+  somma += num_somma;
+  var media = somma / 10;
+}
+
+/* stampo somma e media */
+console.log(somma)
+console.log(media)
